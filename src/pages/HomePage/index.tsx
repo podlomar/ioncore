f493d8d919716adde650b9576599b93fbe533ddf
Martin Podloucký <podlouckymartin@gmail.com>
2025-09-27 18:20:53 +0200

    Add Layout component and styles; update HomePage to use Layout

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,10 @@
 import { JSX } from "react";
+import { Layout } from "../../components/Layout";
 
 export const HomePage = (): JSX.Element => {
-  return <h1>Home Page</h1>;
+  return (
+    <Layout title="Home Page">
+      <h1>Home Page</h1>
+    </Layout>
+  );
 };
